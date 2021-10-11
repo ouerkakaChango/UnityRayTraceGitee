e1c3f702015b7f89b360b9a6544d0f4efd7e8fb8
ouerkakaChango <2293933523@qq.com>
2021-10-11 17:32:33 +0800

    shadeTask待改

--- TestRayTrace/Assets/Scripts/testRT.cs
+++ TestRayTrace/Assets/Scripts/testRT.cs
@@ -16,8 +16,22 @@ public struct HitInfo
     public Vector3 hitP;
 }
 
+public struct ShadeTask
+{
+    public int obj;
+    public Vector3 N;
+    public Vector3 V;
+
+    public Vector3[] Li;
+    public Vector3[] L;
+}
+
 public class testRT : MonoBehaviour
 {
+    public int TraceTime = 2; //第一次Trace只看见自发光物体（光源），第二次看见光源照出的直接光，第三次包含间接光(2bounce)
+    private int nowTraceTime = 0;
+    public const int SSP = 64;
+
     public ComputeShader cs;
     public RenderTexture rt;
 
@@ -26,6 +40,7 @@ public class testRT : MonoBehaviour
 
     private Ray[] rays;
     private HitInfo[] hitInfos;
+    private ShadeTask[] shadeTasks;
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +53,28 @@ public class testRT : MonoBehaviour
 
     }
 
-   private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    int GetRayStride()
+    {//Ray
+     //int colorSize = sizeof(float) * 4;
+        int vector3Size = sizeof(float) * 3;
+        return 2 * vector3Size;
+    }
+
+    int GetHitInfoStride()
+    {//HitInfo
+        int intSize = sizeof(int);
+        int vector3Size = sizeof(float) * 3;
+        return intSize * 2 + vector3Size * 2;
+    }
+
+    int GetShadeTaskStride()
+    {//ShadeTask
+        int intSize = sizeof(int);
+        int vector3Size = sizeof(float) * 3;
+        return intSize + vector3Size * 2 + vector3Size * SSP * 2;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
    {
         if (rt == null)
         {
@@ -68,6 +104,18 @@ public class testRT : MonoBehaviour
                 CreateHitInfo(i, j);
             }
         }
+
+        //shadeTasks = new ShadeTask[w * h * TraceTime];
+        //for (int k = 0; k < TraceTime; k++)
+        //{
+        //    for (int j = 0; j < h; j++)
+        //    {
+        //        for (int i = 0; i < w; i++)
+        //        {
+        //            CreateShadeTask(i, j, k);
+        //        }
+        //    }
+        //}
     }
 
     void CreateRay(int i, int j)
@@ -88,21 +136,19 @@ public class testRT : MonoBehaviour
         hitInfos[i + w * j] = hitInfo;
     }
 
-    int GetRayStride()
-    {//Ray
-        //int colorSize = sizeof(float) * 4;
-        int vector3Size = sizeof(float) * 3;
-        return 2 * vector3Size;
+    void CreateShadeTask(int i, int j,int k)
+    {
+        ShadeTask shadeTask = new ShadeTask();
+        shadeTask.obj = -1;
+        shadeTask.N = new Vector3();
+        shadeTask.V = new Vector3();
+
+        shadeTask.L = new Vector3[SSP];
+        shadeTask.Li = new Vector3[SSP];
+        shadeTasks[i + w * j + w * h * k] = shadeTask;
     }
 
-    int GetHitInfoStride()
-    {//HitInfo
-        int intSize = sizeof(int);
-        int vector3Size = sizeof(float) * 3;
-        return intSize*2 + vector3Size*2;
-    }
-
-    void Bounce()
+    void Trace()
     {
         //??? make sure has inited
 
@@ -112,7 +158,7 @@ public class testRT : MonoBehaviour
         ComputeBuffer buffer_hitInfos = new ComputeBuffer(w * h, GetHitInfoStride());
         buffer_hitInfos.SetData(hitInfos);
 
-        int kInx = cs.FindKernel("Bounce");
+        int kInx = cs.FindKernel("Trace");
 
         cs.SetBuffer(kInx, "rays", buffer_rays);
         cs.SetBuffer(kInx, "hitInfos", buffer_hitInfos);
@@ -129,6 +175,40 @@ public class testRT : MonoBehaviour
         buffer_hitInfos.Dispose();
     }
 
+    void Bounce()
+    {
+        //??? make sure has inited
+
+        ComputeBuffer buffer_rays = new ComputeBuffer(w * h, GetRayStride());
+        buffer_rays.SetData(rays);
+
+        ComputeBuffer buffer_hitInfos = new ComputeBuffer(w * h, GetHitInfoStride());
+        buffer_hitInfos.SetData(hitInfos);
+
+        //ComputeBuffer buffer_shadeTasks = new ComputeBuffer(w * h * TraceTime, GetShadeTaskStride());
+        //buffer_shadeTasks.SetData(shadeTasks);
+
+        int kInx = cs.FindKernel("Bounce");
+
+        cs.SetBuffer(kInx, "rays", buffer_rays);
+        cs.SetBuffer(kInx, "hitInfos", buffer_hitInfos);
+        //cs.SetBuffer(kInx, "shadeTasks", buffer_shadeTasks);
+        cs.SetTexture(kInx, "Result", rt);
+        cs.SetFloat("w", w);
+        cs.SetFloat("h", h);
+
+        cs.Dispatch(kInx, w / 8, h / 8, 1);
+
+        buffer_rays.GetData(rays);
+        buffer_rays.Dispose();
+
+        buffer_hitInfos.GetData(hitInfos);
+        buffer_hitInfos.Dispose();
+
+        //buffer_shadeTasks.GetData(shadeTasks);
+        //buffer_shadeTasks.Dispose();
+    }
+
     void Render()
     {
         //??? make sure has inited,bounced
@@ -139,7 +219,8 @@ public class testRT : MonoBehaviour
         ComputeBuffer buffer_hitInfos = new ComputeBuffer(w * h, GetHitInfoStride());
         buffer_hitInfos.SetData(hitInfos);
 
-        int kInx = cs.FindKernel("Render");
+        //int kInx = cs.FindKernel("Render");
+        int kInx = cs.FindKernel("Render_MonteCarlo");
 
         cs.SetBuffer(kInx, "rays", buffer_rays);
         cs.SetBuffer(kInx, "hitInfos", buffer_hitInfos);
@@ -190,7 +271,11 @@ public class testRT : MonoBehaviour
             CreateRays();
             InitRays();
         } 
-        if (GUI.Button(new Rect(0, 50, 100, 50), "Bounce"))
+        if (GUI.Button(new Rect(0, 50, 100, 50), "Trace"))
+        {
+            Trace();
+        }
+        if (GUI.Button(new Rect(100, 50, 100, 50), "Bounce"))
         {
             Bounce();
         }
