462fc43160d58c690982121d6934f70fd291a365
ouerkakaChango <2293933523@qq.com>
2021-10-22 17:56:12 +0800

    调整衰减看效果

--- TestRayTrace/Assets/HLSL/Render/Light.hlsl
+++ TestRayTrace/Assets/HLSL/Render/Light.hlsl
@@ -8,20 +8,20 @@ float3 GetAttenuationed(float3 lightColor, float3 pos, float3 lightPos)
 {
 	float ldis2 = dis2(pos, lightPos);
 	float attenuation = 1.0;
-	//attenuation = 1 / ldis2;
-	//attenuation = saturate(attenuation);
+	attenuation = 1 / ldis2;
+	attenuation = saturate(attenuation);
 	//???
 	//attenuation = 1;
 	//按光学原理， atten 正比 1/dis2
 	//1.防止距离太近的时候除爆了，衰减亮度需要一个最小值
 	//2.可以调整衰减速度，并且保证[0,1]
 	//float d2min = 0.001;
-	//float d2max = 200;
+	//float d2max = 50;
 	//if (ldis2 > d2min)
 	//{
 	//	attenuation = (d2max - ldis2) / (d2max - d2min);
 	//}
-	//attenuation = saturate(attenuation);
+	attenuation = saturate(attenuation);
 
 	return attenuation * lightColor;
 }
--- TestRayTrace/Assets/Scripts/PathTrace.cs
+++ TestRayTrace/Assets/Scripts/PathTrace.cs
@@ -71,7 +71,7 @@ public class PathTrace : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        DoPathTrace();
     }
 
     // Update is called once per frame
@@ -384,9 +384,9 @@ public class PathTrace : MonoBehaviour
         //{
         //    Render();
         //}
-        if (GUI.Button(new Rect(0, 50, 100, 50), "Do"))
-        {
-            DoPathTrace();
-        } 
+        //if (GUI.Button(new Rect(0, 50, 100, 50), "Do"))
+        //{
+        //    DoPathTrace();
+        //} 
     }
 }
