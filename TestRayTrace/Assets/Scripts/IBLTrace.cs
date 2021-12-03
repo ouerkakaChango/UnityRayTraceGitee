b0c5ee45af7ee2775156a1766a94db0cd20d895e
ouerkakaChango <2293933523@qq.com>
2021-12-03 14:56:17 +0800

    初步融合Mesh和HDR

---
+++ TestRayTrace/Assets/Scripts/IBLTrace.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FastGeo;
+using Ray = FastGeo.Ray;
+using Debugger;
+
+public class IBLTrace : MonoBehaviour
+{
+    //根据网上资料，N卡最小也有32threads，A卡64
+    //所以 Use 64 threads (e.g. (64, 1, 1), (8, 8, 1), (16, 2, 2) etc.)
+    //threads数量过低会慢很多
+    const int CoreX = 8;
+    const int CoreY = 8;
+
+    Vertex[] vertices=null;
+    int[] tris=null;
+
+    ComputeBuffer buffer_tris;
+    ComputeBuffer buffer_vertices;
+
+    public ComputeShader cs;
+    RenderTexture rt;
+    public Texture2D envTex;
+    RenderTexture envRT=null;
+
+    public int w = 1024;
+    public int h = 720;
+
+    Vector3 eyePos;
+    Vector3 screenLeftDownPix;
+    Vector3 screenU;
+    Vector3 screenV;
+    float pixW;
+    float pixH;
+
+    public GameObject meshObj;
+    Mesh mesh;
+
+    void Start()
+    {
+        var meshFiliter = meshObj.GetComponent<MeshFilter>();
+        mesh = meshFiliter.mesh;
+
+        UpdateCamParam();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (rt == null)
+        {
+            rt = new RenderTexture(w, h, 24);
+            rt.enableRandomWrite = true;
+            rt.Create();
+        }
+        Graphics.Blit(rt, destination);
+    }
+
+    private void OnDisable()
+    {
+        SafeDispose(buffer_tris);
+        SafeDispose(buffer_vertices);
+    }
+
+    //##################################################################################################
+    void UpdateCamParam()
+    {
+        var cam = gameObject.GetComponent<Camera>();
+        var near = cam.nearClipPlane;
+        var far = cam.farClipPlane;
+
+        var camPos = gameObject.transform.position;
+        var camForward = gameObject.transform.forward;
+        eyePos = camPos;
+        var screenPos = camPos + near * camForward;
+        screenU = gameObject.transform.right;
+        screenV = gameObject.transform.up;
+
+        //大概在Unity场景中对比了一下渲染大小，定下了合理的像素晶元大小（也就是根据了w,h和原始的cam nf,FOV,尝试出合适的pixW）
+        pixW = 0.000485f;
+        pixH = pixW;
+        screenLeftDownPix = screenPos + screenU * (-w / 2.0f + 0.5f) * pixW + screenV * (-h / 2.0f + 0.5f) * pixH;
+    }
+
+    static public void PreComputeBuffer(ref ComputeBuffer buffer, int stride, in System.Array dataArr)
+    {
+        if (buffer != null)
+        {
+            return;
+        }
+        buffer = new ComputeBuffer(dataArr.Length, stride);
+        buffer.SetData(dataArr);
+    }
+
+    static public void PostComputeBuffer(ref ComputeBuffer buffer, System.Array arr)
+    {
+        return;
+        //buffer.GetData(arr);
+        //buffer.Dispose();
+    }
+
+    static public int GetTriStride()
+    {
+        int intSize = sizeof(int);
+        return intSize;
+    }
+
+    static public int GetVertexStride()
+    {
+        int vec3Size = sizeof(float) * 3;
+        return 2 * vec3Size;
+    }
+
+    static public int GetRayStride()
+    {
+        int vec3Size = sizeof(float) * 3;
+        return 2 * vec3Size;
+    }
+
+    //################################################################################################################
+    void Compute_Render()
+    {
+        PreComputeBuffer(ref buffer_vertices, GetVertexStride(), vertices);
+        PreComputeBuffer(ref buffer_tris, GetTriStride(), tris);
+        //##################################
+        //### compute
+        int kInx = cs.FindKernel("Render");
+
+        cs.SetBuffer(kInx, "tris", buffer_tris);
+        cs.SetBuffer(kInx, "vertices", buffer_vertices);
+
+        cs.SetTexture(kInx, "Result", rt);
+        cs.SetTexture(kInx, "envRT", envRT);
+        cs.SetInt("w", w);
+        cs.SetInt("h", h);
+        cs.SetInt("triNum", tris.Length);
+        cs.SetFloat("pixW", pixW);
+        cs.SetFloat("pixH", pixH);
+        cs.SetVector("screenLeftDownPix", screenLeftDownPix);
+        cs.SetVector("eyePos", eyePos);
+        cs.SetVector("screenU", screenU);
+        cs.SetVector("screenV", screenV);
+
+        cs.Dispatch(kInx, w / CoreX, h / CoreY, 1);
+        //### compute
+        //#####################################
+        //PostComputeBuffer(ref buffer_vertices, vertices);
+        //PostComputeBuffer(ref buffer_tris, tris);
+    }
+    //####################################################################################
+    void CreateMesh()
+    {
+        tris = mesh.triangles;
+
+        int vertCount = mesh.vertices.Length;
+        var l2w = meshObj.transform.localToWorldMatrix;
+        var rot = meshObj.transform.rotation;
+        vertices = new Vertex[vertCount];
+        for (int i = 0; i < vertCount; i++)
+        {
+            Vertex v = new Vertex();
+            v.p = l2w.MultiplyPoint(mesh.vertices[i]);
+            v.n = rot * (mesh.normals[i]);
+            vertices[i] = v;
+        }
+    }
+
+    void Init()
+    {
+        if (envRT == null)
+        {
+            Tex2RT(ref envRT, envTex);
+        }
+        if (vertices == null)
+        {
+            CreateMesh();
+        }
+    }
+
+    void DoRender()
+    {
+        Compute_Render();
+    }
+    //####################################################################################
+
+    static public void SafeDispose(ComputeBuffer cb)
+    {
+        if (cb != null)
+        {
+            cb.Dispose();
+        }
+    }
+
+    static public void Tex2RT(ref RenderTexture rt, Texture2D tex, bool enableRW=true)
+    { 
+        rt = new RenderTexture(tex.width, tex.height, 24);
+        if (enableRW)
+        {
+            rt.enableRandomWrite = true;
+        }
+        var ori = RenderTexture.active;
+
+        RenderTexture.active = rt;
+
+        Graphics.Blit(tex, rt);
+        rt.Create();
+
+        RenderTexture.active = ori;
+    }
+
+    IEnumerator Co_GoIter;
+    //@@@
+    private void OnGUI()
+    {
+
+        if (GUI.Button(new Rect(0, 0, 100, 50), "GoRender!"))
+        {
+            Co_GoIter = GoIter();
+            StartCoroutine(Co_GoIter);
+        }
+    }
+
+    IEnumerator GoIter()
+    {
+        while(true)
+        {
+            Init();
+            UpdateCamParam();
+            DoRender();
+            yield return null;
+        }
+    }
+}
