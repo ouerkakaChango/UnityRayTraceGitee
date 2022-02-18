20509ce85298bd4046d58ba01c32cad545c66b7e
ouerkakaChango <2293933523@qq.com>
2022-02-18 12:15:55 +0800

    移植FSR RCAS，测试成功

--- TestRayTrace/Assets/Scripts/Tools/ComputeShaderTool/FSRProcessor.cs
+++ TestRayTrace/Assets/Scripts/Tools/ComputeShaderTool/FSRProcessor.cs
@@ -5,14 +5,31 @@ using static CopyUtility;
 
 public class FSRProcessor : ImgCSProcessor
 {
-    protected override void PrepareRT(ref Texture2D tex)
+    public string kernel2 = "RCAS";
+    public float sharpness = 0.9f;
+    public float scale = 2.0f;
+    public RenderTexture easuRT;
+    protected override void PrepareRT(ref Texture2D tex, ref RenderTexture rTex)
     {
-        Debug.Log("FSR Create RT");
-        int w = tex.width * 2;
-        int h = tex.height * 2;
-        rt = new RenderTexture(w, h, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
-        rt.enableRandomWrite = true;
+        //Debug.Log("FSR Create RT");
+        int w = (int)(tex.width * scale);
+        int h = (int)(tex.height * scale);
+        rTex = new RenderTexture(w, h, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        rTex.enableRandomWrite = true;
 
-        rt.Create();
+        rTex.Create();
+    }
+
+    protected override void Compute_Process(ref Texture2D tex, ref RenderTexture rTex)
+    {
+        Debug.Log("FSR Compute");
+        base.Compute_Process(ref tex, ref easuRT);
+        int w = rt.width;
+        int h = rt.height;
+        int kInx = cs.FindKernel(kernel2);
+        cs.SetTexture(kInx, "easuRT", easuRT);
+        cs.SetTexture(kInx, "Result", rt);
+        cs.SetFloat("sharpness", sharpness);
+        cs.Dispatch(kInx, w / 8, h / 8, 1);
     }
 }
--- TestRayTrace/Assets/Scripts/Tools/ComputeShaderTool/ImgCSProcessor.cs
+++ TestRayTrace/Assets/Scripts/Tools/ComputeShaderTool/ImgCSProcessor.cs
@@ -27,24 +27,24 @@ public class ImgCSProcessor : MonoBehaviour
     {
         var mat = SetMaterial(gameObject, templateObj);
         Texture2D tex = (Texture2D)templateObj.GetComponent<MeshRenderer>().sharedMaterial.GetTexture("_MainTex");
-        Compute_Process(ref tex);
+        Compute_Process(ref tex, ref rt);
         mat.SetTexture("_MainTex", rt);
     }
 
-    protected virtual void PrepareRT(ref Texture2D tex)
+    protected virtual void PrepareRT(ref Texture2D tex, ref RenderTexture rTex)
     {
-        CreateRT(ref rt, ref tex);
+        CreateRT(ref rTex, ref tex);
     }
 
-    void Compute_Process(ref Texture2D tex)
+    protected virtual void Compute_Process(ref Texture2D tex, ref RenderTexture rTex)
     {
         //Debug.Log(tex);
-        PrepareRT(ref tex);
+        PrepareRT(ref tex, ref rTex);
         int w = rt.width;
         int h = rt.height;
         int kInx = cs.FindKernel(kernel);
         cs.SetTexture(kInx, "inTex", tex);
-        cs.SetTexture(kInx, "Result", rt);
+        cs.SetTexture(kInx, "Result", rTex);
         cs.Dispatch(kInx, w/8, h/8, 1);
     }
 }
