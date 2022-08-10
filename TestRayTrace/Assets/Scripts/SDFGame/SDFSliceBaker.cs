11944cf6ec1a6a8fa30acb482b7c714116c910c7
ouerkakaChango <2293933523@qq.com>
2022-08-10 17:46:57 +0800

    准备对图片slice进行离线bakeSDF

---
+++ TestRayTrace/Assets/Scripts/SDFGame/SDFSliceBaker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ImageUtility;
+
+public class SDFSliceBaker : MonoBehaviour
+{
+    public Texture inputTex;
+    public ColorChannel targetChannel = ColorChannel.R;
+    public bool blackOrWhite = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TestRayTrace/Assets/Scripts/Utility/ImageUtility.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImageUtility
+{
+    public enum ColorChannel
+    {
+        R,G,B,A,
+    }
+
+}
